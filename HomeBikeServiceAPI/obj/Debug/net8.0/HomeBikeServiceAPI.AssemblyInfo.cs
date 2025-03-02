78e42c8d846a2900e99f264afa6816f969dad681
Ayub Bhatta <bhatta.ayub1263@gmail.com>
2025-03-02 09:53:21 +0545

    new

--- HomeBikeServiceAPI/obj/Debug/net8.0/HomeBikeServiceAPI.AssemblyInfo.cs
+++ HomeBikeServiceAPI/obj/Debug/net8.0/HomeBikeServiceAPI.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d7965dec-c5ce-417a-833f-4d1f9bd39c2d")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeBikeServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32054b51cc4b06bf479ef8e500dc316435f79d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeBikeServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeBikeServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
